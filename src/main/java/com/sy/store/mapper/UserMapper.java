746ef2e3c034263571cc8d8bd920b5e54049eef1
xiaoke <yjmqaz@126.com>
2022-01-08 15:45:58 +0800

    first commit

---
+++ src/main/java/com/sy/store/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.sy.store.mapper;
+
+import com.sy.store.entity.User;
+
+/**
+ * 处理用户数据操作的持久层接口
+ *
+ * @author xiaoke
+ */
+public interface UserMapper {
+    /**
+     * 插入用户数据
+     *
+     * @param user 用户数据
+     * @return 受影响的行数
+     */
+    Integer insert(User user);
+
+    /**
+     * 根据用户名查询用户数据
+     *
+     * @param username 用户名
+     * @return 如果匹配成功就返回匹配的用户数据，如果没有成功就返回null
+     */
+    User findByUsername(String username);
+}
